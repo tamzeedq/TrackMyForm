50459f1efe275619a6ee309aa2e7c0f41b28a423
tamzeedq <74509993+tamzeedq@users.noreply.github.com>
2023-12-25 13:05:50 -0800

    added some buttons and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, {useState, useEffect, useRef} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import React, { useState, useEffect, useRef } from 'react';
 import * as poseDetection from '@tensorflow-models/pose-detection';
 import * as tf from '@tensorflow/tfjs-core';
 import '@tensorflow/tfjs-backend-webgl';
 import Webcam from 'react-webcam';
+import { FaCloudUploadAlt } from 'react-icons/fa';
 
 function App() {
   const webcamRef = useRef<Webcam>(null);
@@ -14,9 +14,10 @@ function App() {
   useEffect(() => {
     const initializeTensorFlow = async () => {
       await tf.ready();
+      const detectorConfig = { modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING };
       const detectorInstance = await poseDetection.createDetector(
         poseDetection.SupportedModels.MoveNet,
-        { modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING }
+        detectorConfig
       );
       setPoseDetector(detectorInstance);
     };
@@ -24,49 +25,75 @@ function App() {
     initializeTensorFlow();
   }, []);
 
-  const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING};
-  const detector = async() => {
-    const poseModel = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
-    return poseModel;
-  };
-
   const detect = async () => {
     if (webcamRef.current && webcamRef.current.video && webcamRef.current.video.readyState === 4) {
       const video = webcamRef.current.video;
       if (poseDetector) {
         const poses = await poseDetector.estimatePoses(video);
-        console.log(poses); // Handle the detected poses as needed
+        console.log(poses);
+
+        drawPoses(poses, video.width, video.height);
       }
     }
-  }
+  };
 
-  useEffect(() => {
-    const detectionInterval = setInterval(() => {
-      detect();
-    }, 500); // Run every 0.5 seconds
+  const drawPoses = (poses: poseDetection.Pose[], videoWidth: number, videoHeight: number) => {
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext('2d');
+      if (ctx) {
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+        ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
 
-    return () => clearInterval(detectionInterval); // Cleanup interval on component unmount
-  }, [detect]);
+        // Loop through each detected pose and draw it on the canvas
+        poses.forEach((pose) => {
+          pose.keypoints.forEach((keypoint) => {
+            const { x, y } = keypoint;
+            const circleColor = 'red';
+            ctx.fillStyle = circleColor;
+            ctx.beginPath();
+            ctx.arc(x, y, 10, 0, 2 * Math.PI);
+            ctx.fill();
+          });
+        });
+      }
+    }
+  };
+
+  // useEffect(() => {
+  //   const detectionInterval = setInterval(() => {
+  //     detect();
+  //   }, 500); // Run every 0.5 seconds
+
+  //   return () => clearInterval(detectionInterval); // Cleanup interval on component unmount
+  // }, [detect]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link" 
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        {/* <p>hello world</p> */}
-        <Webcam ref={webcamRef} className='absolute mx-auto z-9 text-center w-640 h-480'></Webcam>
-        <canvas ref={canvasRef} className='absolute mx-auto text-center w-640 h-480'></canvas>
-      </header>
+    <div className="min-h-screen bg-zinc-800 flex justify-center items-center">
+      <div className="min-h-screen bg-zinc-800 flex flex-col justify-around">
+        <div className="text-center">
+          <h1 className="text-white text-6xl font-bold">Track My Form</h1>
+        </div>
+        <div className="m-0 p-0 relative text-center">
+          <Webcam ref={webcamRef} className="z-5 m-0 p-0 w-[640px] h-[480px] border-dashed border-2 border-sky-50" />
+          <canvas ref={canvasRef} className="bg-black z-10 absolute top-0 left-0 m-0 p-0 w-[640px] h-[480px]" />
+        </div>
+        <div className="flex flex-row justify-around">
+          <button className="btn bg-zinc-500 outline-none text-white font-bold hover:bg-zinc-700" >
+            RECORD
+          </button>
+          <button className="btn bg-zinc-500 outline-none text-white font-bold hover:bg-zinc-700" >
+            DOWNLOAD
+          </button>
+          <button className="btn bg-zinc-500 outline-none text-white font-bold hover:bg-zinc-700" >
+            SCREENSHOT
+          </button>
+          <label htmlFor="file-input" className="btn bg-zinc-500 outline-none text-white font-bold hover:bg-zinc-700">
+            UPLOAD FILE
+          </label>
+          <input id="file-input" type="file" style={{ display: 'none' }}  />
+        </div>
+      </div>
     </div>
   );
 }
